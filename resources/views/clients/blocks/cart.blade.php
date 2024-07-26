0b0d7a2e96a4ae198f9facc75f731134c5d019d2
buingocdodev04@gmail.com <8.12a5.dobuingoc@gmail.com>
2024-07-26 09:35:18 +0700

    update

--- resources/views/clients/blocks/cart.blade.php
+++ resources/views/clients/blocks/cart.blade.php
@@ -1,8 +1,9 @@
 <div class="aside aside_right overflow-hidden cart-drawer" id="cartDrawer">
     <div class="aside-header d-flex align-items-center">
-      <h3 class="text-uppercase fs-6 mb-0">SHOPPING BAG ( <span class="cart-amount js-cart-items-count">1</span> ) </h3>
+      <h3 class="text-uppercase fs-6 mb-0">SHOPPING BAG ( <span class="cart-amount js-cart-items-count">0</span> ) </h3>
       <button class="btn-close-lg js-close-aside btn-close-aside ms-auto"></button>
     </div><!-- /.aside-header -->
+    @if(Auth::check())
 
     <div class="aside-content cart-drawer-items-list">
       <div class="cart-drawer-item d-flex position-relative">
@@ -79,14 +80,20 @@
       </div><!-- /.cart-drawer-item d-flex -->
 
     </div><!-- /.aside-content -->
-
+    @else 
+    <div class="text-center aside-content cart-drawer-items-list">
+      <h2>Chưa có sản phẩm nào tồn tại</h2>
+        <img width="200" height="200" src="https://static.vecteezy.com/system/resources/previews/005/006/007/non_2x/no-item-in-the-shopping-cart-click-to-go-shopping-now-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg" alt="">
+    </div>
     <div class="cart-drawer-actions position-absolute start-0 bottom-0 w-100">
       <hr class="cart-drawer-divider">
       <div class="d-flex justify-content-between">
         <h6 class="fs-base fw-medium">SUBTOTAL:</h6>
-        <span class="cart-subtotal fw-medium">$176.00</span>
+        <span class="cart-subtotal fw-medium">0 đ</span>
       </div><!-- /.d-flex justify-content-between -->
       <a href="shop_cart.html" class="btn btn-light mt-3 d-block">View Cart</a>
       <a href="shop_checkout.html" class="btn btn-primary mt-3 d-block">Checkout</a>
-    </div><!-- /.aside-content -->
+    </div>
+    @endif
+   <!-- /.aside-content -->
   </div><!
